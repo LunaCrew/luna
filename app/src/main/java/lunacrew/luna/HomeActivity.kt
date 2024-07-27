40e7b205437e6f29c812596b1c81fea0a45e8096
Ash Trindade <ashtrindade5@gmail.com>
2024-07-27 16:22:13 -0300

    chore: add translations

--- app/src/main/java/lunacrew/luna/HomeActivity.kt
+++ app/src/main/java/lunacrew/luna/HomeActivity.kt
@@ -5,16 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 
-class MainActivity : ComponentActivity() {
+class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -26,8 +26,8 @@ class MainActivity : ComponentActivity() {
 fun HomeScreen() {
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Text(
-            "Luna",
-            style = MaterialTheme.typography.headlineLarge,
+            stringResource(id = R.string.app_name),
+            style = typography.titleLarge,
             color = Color.Blue
         )
     }
